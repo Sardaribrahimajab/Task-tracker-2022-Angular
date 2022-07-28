5e8aac18ec0f7afb054a6be4f75d9650899bf10e
Sardaribrahimajab <isardar784@gmail.com>
2022-07-28 01:14:50 -0700

    feat: task tracker

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -10,16 +10,23 @@ import { faTimes } from '@fortawesome/free-solid-svg-icons'
 export class TaskItemComponent implements OnInit {
 @Input() task:Task|undefined;
 @Output() onDeleteTask: EventEmitter<Task>=new EventEmitter();
+@Output() onToggleReminder: EventEmitter<Task>=new EventEmitter();
 text: string = '';
 day: string | undefined = '';
+reminder: boolean |undefined = false;
 faTimes = faTimes;
   constructor() { }
 
   ngOnInit(): void {
     this.text = this.task?.text;
     this.day = this.task?.day;
+    this.reminder =this.task?.reminder;
   }
   onDelete(task:any){
     this.onDeleteTask.emit(task);
   }
+  onToggle(task:any){
+    this.onToggleReminder.emit(task);
+  }
+
 }
